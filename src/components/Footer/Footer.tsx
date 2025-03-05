ad67924956ba9fc2ce47f8aa63e46d85141b3894
cpalmer-ios <write.to.chris.palmer@gmail.com>
2025-03-05 01:54:55 +0000

    added footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { Box, Container, Typography, IconButton, Link as MuiLink, Grid, useTheme } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  IconButton,
+  Link as MuiLink,
+  Grid,
+  useTheme,
+} from "@mui/material";
 import { Facebook, Twitter, Instagram, LinkedIn } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo512.png";
@@ -7,20 +15,23 @@ import logo from "../../assets/logo512.png";
 const Footer = () => {
   const theme = useTheme();
   const isDarkMode = theme.palette.mode === "dark";
-  
+
   return (
     <Box
       component="footer"
       sx={{
-        bgcolor: (theme) => theme.palette.mode === "light" ? "#FFF" : "#212121", // Light background
+        bgcolor: (theme) =>
+          theme.palette.mode === "light" ? "#FFF" : "#212121", // Light background
         py: 4,
+        borderTop: "1px solid rgba(255, 255, 255, 0.12)",
+        mt: "1px",
       }}
     >
       <Container maxWidth="xl">
         <Grid container alignItems="center" justifyContent="space-between">
           {/* Left Section - Logo + Name */}
           <Grid item sx={{ display: "flex", alignItems: "center" }}>
-          <Link
+            <Link
               to="/"
               style={{
                 textDecoration: "none",
@@ -36,7 +47,7 @@ const Footer = () => {
                   style={{
                     width: "20px",
                     height: "20px",
-                    marginTop: '7px',
+                    marginTop: "7px",
                     marginRight: 10,
                     marginLeft: 10,
                     filter: isDarkMode ? "invert(1)" : "none",
@@ -47,7 +58,7 @@ const Footer = () => {
                   component="div"
                   sx={{
                     textAlign: { xs: "center", sm: "left" },
-                    marginTop: '3.5px',
+                    marginTop: "3.5px",
                     flexGrow: 1,
                     fontFamily: "cardo",
                     fontWeight: 600,
@@ -62,41 +73,107 @@ const Footer = () => {
               </Box>
             </Link>
           </Grid>
-
+          <Box width={'100%'}>
+          <Typography variant="body2" sx={{ ml: 1.5, mt: 1, position: 'absolute', color: "grey.600", textAlign: 'left' }}>
+            info@revelationary.org
+          </Typography>
+          </Box>
+          </Grid>
+          <Grid container alignItems="right" justifyContent="flex-end">
           {/* Center Section - Navigation Links */}
           <Grid item sx={{ display: "flex", gap: 4 }}>
-            {["Home", "About", "Resources", "Contact"].map((text) => (
-              <MuiLink
-                key={text}
-                href="#"
-                underline="none"
-                sx={{
-                  color: "#1E40AF",
-                  fontWeight: 500,
-                  "&:hover": { textDecoration: "underline", color: "#2563EB" },
-                }}
-              >
-                {text}
-              </MuiLink>
-            ))}
+            <MuiLink
+              href="https://revelationary.org/"
+              underline="none"
+              sx={{
+                color: "#A1A1A1",
+                fontWeight: 500,
+                "&:hover": {
+                  textDecoration: "underline",
+                  color: (theme) =>
+                    theme.palette.mode === "light" ? "#212121" : "#FFF",
+                },
+              }}
+            >
+              Home
+            </MuiLink>
+            <MuiLink
+              href="https://revelationary.org/about"
+              underline="none"
+              sx={{
+                color: "#A1A1A1",
+                fontWeight: 500,
+                "&:hover": {
+                  textDecoration: "underline",
+                  color: (theme) =>
+                    theme.palette.mode === "light" ? "#212121" : "#FFF",
+                },
+              }}
+            >
+              About
+            </MuiLink>
+            <MuiLink
+              href="https://revelationary.org/resources"
+              underline="none"
+              sx={{
+                color: "#A1A1A1",
+                fontWeight: 500,
+                "&:hover": {
+                  textDecoration: "underline",
+                  color: (theme) =>
+                    theme.palette.mode === "light" ? "#212121" : "#FFF",
+                },
+              }}
+            >
+              Resources
+            </MuiLink>
+            <MuiLink
+              href="https://revelationary.org/contact"
+              underline="none"
+              sx={{
+                color: "#A1A1A1",
+                fontWeight: 500,
+                "&:hover": {
+                  textDecoration: "underline",
+                  color: (theme) =>
+                    theme.palette.mode === "light" ? "#212121" : "#FFF",
+                },
+              }}
+            >
+              Contact
+            </MuiLink>
           </Grid>
         </Grid>
 
         {/* Divider Line */}
-        <Box sx={{ borderBottom: "1px solid", borderColor: "grey.300", my: 3 }} />
+        <Box
+          sx={{ borderBottom: "1px solid", borderColor: "grey.300", my: 3 }}
+        />
 
         {/* Bottom Section - Copyright & Social Icons */}
         <Grid container justifyContent="space-between" alignItems="center">
           <Typography variant="body2" sx={{ color: "grey.600" }}>
-            © {new Date().getFullYear()} Revelationary. All rights reserved.
+            © {new Date().getFullYear()} Revelationary Ltd. All rights reserved.
+            167-169 Great Portland Street 5th Floor London W1W 5PF . Company
+            Number 16288234
           </Typography>
-
           <Box>
-            {[Facebook, Twitter, Instagram, LinkedIn].map((Icon, index) => (
-              <IconButton key={index} sx={{ color: "#1E40AF", "&:hover": { color: "#2563EB" } }}>
-                <Icon />
-              </IconButton>
-            ))}
+            <IconButton
+              href={"https://www.facebook.com/revelationary.online/"}
+              target="_blank"
+              sx={{ color: "#A1A1A1", "&:hover": { color: (theme) =>
+                theme.palette.mode === "light" ? "#212121" : "#FFF", } }}
+            >
+              <Facebook />
+            </IconButton>
+            <IconButton
+              href={"https://www.instagram.com/revelationary.online/"}
+              target="_blank"
+              sx={{ color: "#A1A1A1", "&:hover": { color: (theme) =>
+                theme.palette.mode === "light" ? "#212121" : "#FFF", } }}
+            >
+              <Instagram />
+            </IconButton>
           </Box>
         </Grid>
       </Container>
