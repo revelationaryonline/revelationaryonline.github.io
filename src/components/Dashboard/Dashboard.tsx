e520360d33c53f313f0ac5ed2e01e52947702d32
cpalmer-ios <write.to.chris.palmer@gmail.com>
2025-03-18 21:51:42 +0000

    remove cmd+v

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -653,11 +653,6 @@ useEffect(() => {
                           key={index}
                           onContextMenu={(e) => handleContextMenu(e, v)}
                           onClick={() => handleVerseSelect(v)}
-                          onKeyDown={(e) => {
-                            if (e.key === "v" && e.metaKey) {
-                              handleVerseSelect(v);
-                            }
-                          }}
                           tabIndex={0}
                           role="button"
                           style={{
