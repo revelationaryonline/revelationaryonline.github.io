8fa7d87bbf8b7d2029d06bc630a3c233873da661
cpalmer-ios <write.to.chris.palmer@gmail.com>
2025-06-15 16:32:53 +0100

    Updates

--- src/components/LoadingSpinner/index.ts
+++
@@ -1,5 +0,0 @@
-import LoadingSpinner from './LoadingSpinner';
-
-// Only export the LoadingSpinner component
-// We're now importing LoadingWrapper directly in App.tsx to avoid circular imports
-export default LoadingSpinner;
--- src/components/Tour/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DashboardTour } from './DashboardTour';
-export { default as DashboardTourWrapper } from './DashboardTourWrapper';
-export { default as TourProvider } from './TourProvider';
